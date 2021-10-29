c568370f97c46cddace0512d1a464a4953742ada
Alexander <Alexander_alatorre1993@hotmail.com>
2021-10-28 19:39:49 -0600

    Added reducers

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import App from './components/App'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import App from "./components/App";
+import "./index.css";
+
+import reducers from "./reducers";
 
 function ColorfulBorder() {
   return (
     <React.Fragment>
-      <ul className='border-container'>
-        <li className='border-item' style={{ background: 'var(--red)' }} />
-        <li className='border-item' style={{ background: 'var(--blue)' }} />
-        <li className='border-item' style={{ background: 'var(--pink)' }} />
-        <li className='border-item' style={{ background: 'var(--yellow)' }} />
-        <li className='border-item' style={{ background: 'var(--aqua)' }} />
+      <ul className="border-container">
+        <li className="border-item" style={{ background: "var(--red)" }} />
+        <li className="border-item" style={{ background: "var(--blue)" }} />
+        <li className="border-item" style={{ background: "var(--pink)" }} />
+        <li className="border-item" style={{ background: "var(--yellow)" }} />
+        <li className="border-item" style={{ background: "var(--aqua)" }} />
       </ul>
     </React.Fragment>
-  )
+  );
 }
 
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <React.Fragment>
+  <Provider store={store}>
     <ColorfulBorder />
     <App />
-  </React.Fragment>,
-  document.getElementById('root')
-)
+  </Provider>,
+  document.getElementById("root")
+);
