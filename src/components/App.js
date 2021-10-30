4bf70c2c349a5767891bca57524a914be3d876e6
Alexander <Alexander_alatorre1993@hotmail.com>
2021-10-29 21:02:11 -0600

    Added multiple components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import * as React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { handleInitialData } from "../actions/shared";
+import LeaderBoard from "./LeaderBoard";
+import { BrowserRouter as Router } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import AddPoll from "./AddPoll";
 
 export default function App() {
   const loading = useSelector((state) => state.authedUser === null);
@@ -10,9 +14,9 @@ export default function App() {
     dispatch(handleInitialData());
   }, [dispatch]);
 
-  return <div className="container">
-    {!loading && <h1>
-      Redux Polls
-    </h1> }
-  </div>;
+  return (
+    <Router>
+      <div className="container">{!loading && <AddPoll />}</div>;
+    </Router>
+  );
 }
---
+++ src/components/LeaderBoard.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { useSelector } from "react-redux";
+
+const LeaderBoard = () => {
+  const users = useSelector((state) => state.users);
+
+  const sorted = Object.keys(users)
+    .map((id) => {
+      const { name, avatarURL, polls } = users[id];
+
+      return {
+        id,
+        name,
+        avatarURL,
+        polls: polls.length,
+        answers: users[id].answers.length,
+      };
+    })
+    .sort((a, b) => b.polls - (a.polls + a.polls));
+
+  console.log("sorted", sorted);
+
+  return (
+    <ul>
+      {sorted.map(({ id, name, polls, answers, avatarURL }) => (
+        <li className="user" key={id}>
+          <img src={avatarURL} alt={`Avatar for ${name}`} />
+          <div>
+            <h1>{name}</h1>
+            <p>{polls} Polls</p>
+            <p>{answers} Answers</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default LeaderBoard;
